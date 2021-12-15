70f35ad62cff91ebb1421a0af1d2787c6af1876e
Akudbilla Daniel <akuddev@gmail.com>
2021-12-15 18:43:28 +0000

    added property page details

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,48 +1,51 @@
 import { useContext } from "react";
-import { Box, Flex, Text, Avatar, Icon } from "@chakra-ui/react";
-import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 import Image from "next/image";
+import { Box, Icon, Flex } from "@chakra-ui/react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Flex justifyContent="center" alignItems="center" margin="1">
+    <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon
         as={FaArrowAltCircleLeft}
-        onClick={scrollPrev}
+        onClick={() => scrollPrev()}
         fontSize="2xl"
         cursor="pointer"
+        d={["none", "none", "none", "block"]}
       />
     </Flex>
   );
 };
+
 const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
 
   return (
-    <Flex justifyContent="center" alignItems="center" margin="1">
+    <Flex justifyContent="center" alignItems="center" marginLeft="1">
       <Icon
-        as={FaArrowAltCircleLeft}
-        onClick={scrollNext}
+        as={FaArrowAltCircleRight}
+        onClick={() => scrollNext()}
         fontSize="2xl"
         cursor="pointer"
+        d={["none", "none", "none", "block"]}
       />
     </Flex>
   );
 };
-const Imagescrollbar = ({ data }) => {
+export default function ImageSrollbar({ data }) {
   return (
     <ScrollMenu
       LeftArrow={LeftArrow}
       RightArrow={RightArrow}
-      style={{ overlow: "hidden" }}
+      style={{ overflow: "hidden" }}
     >
       {data.map((item) => (
         <Box
-          key={item.id}
           width="910px"
+          key={item.id}
           itemId={item.id}
           overflow="hidden"
           p="1"
@@ -54,12 +57,10 @@ const Imagescrollbar = ({ data }) => {
             src={item.url}
             width={1000}
             height={500}
-            sizes="(max-width:500px) 100px, (max-width):1023px 400px,1000px"
+            sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
           />
         </Box>
       ))}
     </ScrollMenu>
   );
-};
-
-export default Imagescrollbar;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
 import Router from "next/router";
+import nProgress from "nprogress";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  nProgress.configure({ showSpinner: false });
+  Router.events.on("routeChangeStart", () => {
+    nProgress.start();
+  });
+
+  Router.events.on("routeChangeStart", () => {
+    nProgress.done();
+  });
   return (
     <>
       <Head>
