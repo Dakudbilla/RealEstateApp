f0ad737af72c78c99a4ce3247fd1030fe9ef8595
Akudbilla Daniel <akuddev@gmail.com>
2021-12-16 10:53:19 +0000

    implemented responsive navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,7 @@ import {
   Box,
   Spacer,
   Text,
+  Button,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { FcMenu, FcHome, FcAbout } from "react-icons/fc";
@@ -23,14 +24,58 @@ const Navbar = () => {
         </Link>
       </Box>
       <Spacer />
+      <Flex justifyItems="stretch" display={{ base: "none", lg: "block" }}>
+        <Link href="/" passHref>
+          <Button
+            mr="4"
+            colorScheme="blue"
+            variant="outline"
+            leftIcon={<FcHome />}
+          >
+            Home
+          </Button>
+        </Link>
+        <Link href="/search" passHref>
+          <Button
+            mr="4"
+            colorScheme="blue"
+            variant="outline"
+            leftIcon={<BsSearch />}
+          >
+            Filter Properties
+          </Button>
+        </Link>
+
+        <Link href="/search?purpose=for-sale" passHref>
+          <Button
+            mr="4"
+            colorScheme="teal"
+            variant="outline"
+            leftIcon={<FcAbout />}
+          >
+            Buy Property
+          </Button>
+        </Link>
+        <Link href="/search?purpose=for-rent" passHref>
+          <Button
+            mr="4"
+            colorScheme="teal"
+            variant="outline"
+            leftIcon={<FcHome />}
+          >
+            Rent Property
+          </Button>
+        </Link>
+      </Flex>
       <Menu>
         <MenuButton
           as={IconButton}
           icon={<FcMenu />}
           variant="outlined"
           color="red.400"
+          display={{ base: "block", lg: "none" }}
         />
-        <MenuList>
+        <MenuList display={{ base: "block", lg: "none" }}>
           <Link href="/" passHref>
             <MenuItem icon={<FcHome />}>Home</MenuItem>
           </Link>
